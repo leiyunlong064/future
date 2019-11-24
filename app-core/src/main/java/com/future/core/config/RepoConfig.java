3aa131720fb01d97ceb565000eb934725d5e11e9
leiyunlong <leiyunlong@leiyunlongdeMacBook-Pro.local>
2019-11-25 00:30:08 +0800

    app-core

---
+++ app-core/src/main/java/com/future/core/config/RepoConfig.java
@@ -0,0 +1,69 @@
+package com.future.core.config;
+
+import com.github.pagehelper.PageInterceptor;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = {"classpath:core/database.properties"}, ignoreResourceNotFound = true)
+@PropertySource(value = {"file://${CONFIG_HOME}/database.properties"}, ignoreResourceNotFound = true)
+@MapperScan(basePackages = "com.future.core", annotationClass = Mapper.class)
+public class RepoConfig {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .url(environment.getProperty("db.url"))
+                .username(environment.getProperty("db.username"))
+                .password(environment.getProperty("db.password"))
+                .driverClassName(environment.getProperty("db.driver.classname"))
+                .build();
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        pageInterceptor.setProperties(new Properties() {{
+            setProperty("helperDialect", "postgresql");
+        }});
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageInterceptor});
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        org.apache.ibatis.session.Configuration configuration = sqlSessionFactory.getConfiguration();
+        configuration.setCallSettersOnNulls(true);
+        configuration.setMapUnderscoreToCamelCase(true);
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ app-core/src/main/java/com/future/core/system/providers/UserSqlProvider.java
@@ -0,0 +1,71 @@
+package com.future.core.system.providers;
+
+import com.future.common.entity.SystemUser;
+import com.future.core.repositories.SQL;
+import org.springframework.util.StringUtils;
+
+public class UserSqlProvider {
+    private String getEntityTable() {
+        return "T_SYSTEM_USER";
+    }
+
+    public String save(SystemUser user) {
+        return new SQL()
+                .INSERT_INTO(getEntityTable())
+                .VALUES("USER_ID", "#{userId}")
+                .VALUES("NAME", "#{name}")
+                .VALUES("MOBILE", "#{mobile}")
+                .VALUES("PASSWORD", "#{password}")
+                .VALUES_IF("STATUS", "#{status}", user.getStatus() != null)
+                .VALUES_IF("CREATED_TIME", "#{createTime}", user.getCreateTime() != null)
+                .VALUES_IF("UPDATE_TIME", "#{updateTime}", user.getUpdateTime() != null)
+                .toString();
+    }
+
+    public String update(SystemUser user) {
+        return new SQL()
+                .UPDATE(getEntityTable())
+                .SET_IF("NAME = #{name}", !StringUtils.isEmpty(user.getName()))
+                .SET_IF("MOBILE = #{mobile}", !StringUtils.isEmpty(user.getMobile()))
+                .SET_IF("PASSWORD = #{password}", !StringUtils.isEmpty(user.getPassword()))
+                .SET_IF("STATUS = #{status}", user.getStatus() != null)
+                .WHERE("USER_ID = #{userId}")
+                .toString();
+    }
+
+    public String delete() {
+        return new SQL()
+                .UPDATE(getEntityTable())
+                .SET("IS_DELETED = 'Y'")
+                .WHERE("USER_ID = #{userId}")
+                .toString();
+    }
+
+    public String findOne() {
+        return new SQL()
+                .SELECT("*")
+                .FROM(getEntityTable())
+                .WHERE("USER_ID = #{userId}")
+                .toString();
+    }
+
+    public String findOneByMobile() {
+        return new SQL()
+                .SELECT("*")
+                .FROM(getEntityTable())
+                .WHERE("MOBILE = #{mobile}")
+                .toString();
+    }
+
+    public String findList(SystemUser user) {
+        return new SQL()
+                .SELECT("*")
+                .FROM(getEntityTable())
+                .WHERE_IF("NAME = #{name}", user.getName() != null)
+                .WHERE_IF("MOBILE = #{mobile}", user.getMobile() != null)
+                .WHERE_IF("STATUS = #{status}", user.getStatus() != null)
+                .WHERE("IS_DELETED = 'N'")
+                .ORDER_BY("CREATE_TIME DESC")
+                .toString();
+    }
+}
